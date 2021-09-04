b47c74e6a3dd530ad296d3949cf5e4bd9326874e
Juan David <JD_20rb@hotmail.com>
2021-09-05 01:29:08 +0200

    coordinate positioning fixed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnim
 
 let svg_path = $('#path')[0];
 let circle = $('#circle')[0];
-let rect = $('#rectangle')[0];
+let line = $('#line')[0];
 
 const b = 0.1;	
 const o = 0;
@@ -20,8 +20,8 @@ let start;
 let x_pos = 0;
 let y_pos = 0;
 
-let dot_x_pos = 50;
-let dot_y_pos = 300;
+let dot_x_pos = 0;
+let dot_y_pos = 0;
 
 
 function f1(x){
@@ -32,53 +32,14 @@ function createDots(x){
 	return f1(x)*Math.cos(.5*w*x + o);
 }
 
-function getSlope(){
-	console.log('(' + x_pos  + ', ' + y_pos + '),(' + dot_x_pos + ', ' + dot_y_pos +')');
-	return (y_pos - dot_y_pos)/(x_pos - dot_x_pos);
-}
-
-function getDotYPos(x) {
-
-	if(dot_y_pos == y_pos)
-		return dot_y_pos;
-
-	let slope = getSlope();
-
-	if(y_pos < dot_y_pos){
-		if(slope > 0)
-			slope = - slope;
-	}else{
-		if(slope < 0)
-			slope = - slope;
-	}
-	console.log(slope);
-
-	return slope*(x + STEP) - slope*dot_x_pos + dot_y_pos;
-}
-
-function getDotXPos(){
-
-	if(x_pos > dot_x_pos){
-		if(dot_x_pos + STEP > x_pos )
-			return x_pos;
-		return dot_x_pos = dot_x_pos + STEP;
-	}
-
-	if(x_pos < dot_x_pos){
-		if(dot_x_pos - STEP < x_pos )
-			return x_pos;
-		return dot_x_pos = dot_x_pos - STEP;
-	}
-
-	return x_pos;
-}
-
 
 function bounce(path, x, y, i){
 	if( i < DURATION){
 
 		dot_x_pos = (createDots(i)*0.25*x)+50;
 		dot_y_pos = (createDots(i)*0.3*y)+300;
+
+		console.log(dot_x_pos);
 
 		path.setAttribute('d','M 50 0 Q ' + dot_x_pos + ' ' + dot_y_pos +' 50 600');
 		
@@ -95,40 +56,54 @@ function bounce(path, x, y, i){
 	}
 }
 
+function getVector(){
+	let vector = [];
+	vector[0] = (x_pos - dot_x_pos);
+	vector[1] = (y_pos - dot_y_pos);
+
+	return normalizeVector(vector);
+}
+
+function normalizeVector(vector){
+	let v = [];
+	let sqrts = Math.pow(vector[0],2) + Math.pow(vector[1],2);
+	let norm = Math.sqrt(sqrts);
+	
+	v[0] = vector[0] / norm;
+	v[1] = vector[1] / norm;
+	return v;
+}
 
 function track(){
-	dot_x_pos = getDotXPos();
 
-	if(dot_x_pos != x_pos)
-		dot_y_pos = getDotYPos(dot_x_pos);
+	if(x_pos != dot_x_pos || y_pos != dot_y_pos){
+		let vector = getVector();
 
-	else{
+		path.setAttribute('d','M 50 0 Q ' + (50 + dot_x_pos) + ' ' + (300 - (dot_y_pos +  vector[1])) +' 50 600');
 
-		if(y_pos > dot_y_pos){
-			if(dot_y_pos + STEP > y_pos )
-				dot_y_pos = y_pos;
-			dot_y_pos = dot_y_pos + STEP;
-		}
+		circle.setAttribute('cx',  50 + dot_x_pos + vector[0]);
+		circle.setAttribute('cy', 300 - (dot_y_pos +  vector[1]));
 
-		if(y_pos < dot_y_pos){
-			if(dot_y_pos - STEP < y_pos )
-				dot_y_pos = y_pos;
-			dot_y_pos = dot_y_pos - STEP;
-		}
+		line.setAttribute('x2', 50 + dot_x_pos + vector[0]);
+		line.setAttribute('y2', 300 - (dot_y_pos +  vector[1]));
 
+		if(Math.abs(dot_x_pos + vector[0] - x_pos) <= 1)
+			dot_x_pos = x_pos;
+		else
+			dot_x_pos =  dot_x_pos + 5*vector[0];
+
+		if(Math.abs(dot_y_pos + vector[1] - y_pos) <= 20)
+			dot_y_pos = y_pos;
+		else
+			dot_y_pos =  dot_y_pos + 20*vector[1];
 	}
-
-	//path.setAttribute('d','M 50 0 Q ' + dot_x_pos + ' ' + dot_y_pos+' 50 600');
-
-	circle.setAttribute('cx', dot_x_pos);
-	circle.setAttribute('cy', dot_y_pos);
 
 	animationId = requestAnimationFrame(track);
 }
 
 $('#box').mousemove((e)=>{
-	x_pos = e.pageX - $(e.currentTarget).offset().left;
-	y_pos = e.pageY - $(e.currentTarget).offset().top;
+	x_pos =  - 50 + (e.pageX - $(e.currentTarget).offset().left);
+	y_pos = 300 - (e.pageY - $(e.currentTarget).offset().top);
 });
 
 $('#box').mouseenter((e)=>{
@@ -140,7 +115,7 @@ $('#box').mouseenter((e)=>{
 $('#box').mouseleave((e)=>{
 	cancelAnimationFrame(animationId);
 
-	//animationId = requestAnimationFrame(()=>{
-	//	bounce(svg_path, dot_x_pos -50, dot_y_pos - 300, 0);
-	//});
+	animationId = requestAnimationFrame(()=>{
+		bounce(svg_path, dot_x_pos -50, dot_y_pos - 300, 0);
+	});
 });
